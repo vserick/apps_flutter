fa35aa4c0450349654764e609c74007120fa2083
Erick Viana <erickvianamf@gmail.com>
2024-08-20 21:32:45 -0300

    implementado paleta de cores simples, e Fields de login.

--- app_futebol/lib/login/login_screen.dart
+++ app_futebol/lib/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -10,7 +11,7 @@ class LoginScreen extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            color: const Color.fromARGB(255, 8, 148, 104),
+              color: const Color.fromARGB(255, 8, 148, 104),
               height: double.infinity,
               width: double.infinity,
               child: Opacity(
@@ -22,30 +23,71 @@ class LoginScreen extends StatelessWidget {
               )),
           SafeArea(
             child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
               height: double.infinity,
               width: double.infinity,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
+                  SizedBox(
                     height: 150,
                     child: Image.asset('assets/images/logo.png'),
                   ),
-                  Container(
-                    height: 150,
-                    // color: Colors.amber,
+                  Text(
+                    'Bem-Vindo ao Boleiros',
+                    style: GoogleFonts.signikaNegative(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.surface,
+                      shadows: [
+                        const Shadow(
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 3.0,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
                   ),
-                  Container(
-                    height: 150,
-                    // color: Colors.amber,
-                  ),
-                  Container(
-                    height: 150,
-                    // color: Colors.amber,
+                  _formFields(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {}, child: const Text('Entrar')),
+                    ],
                   ),
                 ],
               ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _formFields() {
+    return Form(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+                labelText: 'Email',
+                prefixIcon: Icon(
+                  Icons.email,
+                )),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+                labelText: 'Senha',
+                prefixIcon: Icon(
+                  Icons.key,
+                )),
+            obscureText: true,
           ),
         ],
       ),
--- app_futebol/lib/main.dart
+++ app_futebol/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_futebol/injector.dart';
 import 'package:app_futebol/launcher.dart';
+import 'package:app_futebol/theme/color_scheme.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,7 @@ void main() {
   configureDependencies();
 
   runApp(DevicePreview(
-    enabled: !true,
+    enabled: true,
     builder: (context) => const MyApp(),
   ));
 }
@@ -24,9 +25,39 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: ColorApp.primary,
+            secondary: ColorApp.secondary,
+            tertiary: ColorApp.tertiary,
+            error: ColorApp.error,
+            surface: ColorApp.surface,
+          ),
+          elevatedButtonTheme: const ElevatedButtonThemeData(
+              style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(ColorApp.tertiary))),
+          inputDecorationTheme: const InputDecorationTheme(
+              filled: true,
+              fillColor: Color.fromARGB(83, 240, 238, 205),
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+              labelStyle: TextStyle(color: ColorApp.tertiary),
+              floatingLabelStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: ColorApp.tertiary),
+              prefixIconColor: ColorApp.tertiary,
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(),
+                  borderRadius: BorderRadius.all(Radius.circular(50))),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.all(Radius.circular(50))),
+              focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: ColorApp.tertiary),
+                  borderRadius: BorderRadius.all(Radius.circular(50)))),
+          iconTheme:
+              const IconThemeData(color: ColorApp.surface)),
       home: const Launcher(),
     );
   }
